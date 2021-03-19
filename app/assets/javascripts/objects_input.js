34572bce196c684488ab162beb73eae6c0a0b399
Vahid Kiani <vdkkia2004@yahoo.com>
2021-03-19 20:51:06 +0100

    Update to accept free text for ontologies

--- app/assets/javascripts/objects_input.js
+++ app/assets/javascripts/objects_input.js
@@ -1,47 +1,48 @@
 function loadObjectInputs() {
-    $j('[data-role="seek-objectsinput"]').each(function () {
-        var options = { tagClass: 'label label-default',
-            itemValue: 'id',
-            itemText: 'name'
-        };
+  $j('[data-role="seek-objectsinput"]').each(function () {
+    var options = { tagClass: "label label-default", itemValue: "id", itemText: "name" };
 
-        if($j(this).data('tagsLimit'))
-            options.maxTags = $j(this).data('tagsLimit');
+    if ($j(this).data("tagsLimit")) options.maxTags = $j(this).data("tagsLimit");
 
-        if($j(this).data('typeahead')) {
-            var opts = {
-                datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-                queryTokenizer: Bloodhound.tokenizers.whitespace
-            };
+    if ($j(this).data("typeahead")) {
+      var opts = {
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+      };
 
-            if(prefetchUrl = $j(this).data('typeahead-prefetch-url'))
-                opts.prefetch = { url: prefetchUrl };
-            if(queryUrl = $j(this).data('typeahead-query-url'))
-                opts.remote = { url: queryUrl };
-            if(localValues = $j(this).data('typeahead-local-values'))
-                opts.local = localValues;
+      if ((prefetchUrl = $j(this).data("typeahead-prefetch-url"))) opts.prefetch = { url: prefetchUrl };
+      if ((queryUrl = $j(this).data("typeahead-query-url"))) opts.remote = { url: queryUrl };
+      if ((localValues = $j(this).data("typeahead-local-values"))) opts.local = localValues;
 
-            var d = new Bloodhound(opts);
-            d.initialize();
+      var d = new Bloodhound(opts);
+      d.initialize();
 
-            var template = $j(this).data('typeahead-template') || 'typeahead/hint';
+      var template = $j(this).data("typeahead-template") || "typeahead/hint";
 
-            options.typeaheadjs = {
-                displayKey: 'name',
-                source: d.ttAdapter(),
-                templates: {
-                    suggestion: HandlebarsTemplates[template]
-                }
-            };
-        }
+      options.typeaheadjs = {
+        displayKey: "name",
+        source: d.ttAdapter(),
+        templates: {
+          suggestion: HandlebarsTemplates[template],
+        },
+      };
+    }
 
-        $j(this).tagsinput(options);
+    $j(this).tagsinput(options);
 
-        var objects = $j(this).data('existingObjects');
-        if(objects)
-            for(var i = 0; i < objects.length; i++)
-                $j(this).tagsinput('add', objects[i]);
+    var objects = $j(this).data("existingObjects");
+    if (objects) for (var i = 0; i < objects.length; i++) $j(this).tagsinput("add", objects[i]);
+
+    const tagsinput = $j(this).siblings(".bootstrap-tagsinput");
+    const input = $j(tagsinput).find("input.tt-input");
+    const elem = $j(this);
+    $j(tagsinput).on("focusout", function () {
+      $j(elem).tagsinput("add", { id: input.val(), name: input.val() });
+      setTimeout(() => {
+        $j(input).val("");
+      }, 1);
     });
+  });
 }
 
 $j(document).ready(loadObjectInputs);
