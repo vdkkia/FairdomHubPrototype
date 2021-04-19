dbb0460658c1e5e53aa7a909e533f442e36fae44
Vahid Kiani <vdkkia2004@yahoo.com>
2021-04-19 13:40:05 +0200

    Add view permission rule to repository standards

--- app/models/repository_standard.rb
+++ app/models/repository_standard.rb
@@ -1,5 +1,19 @@
 class RepositoryStandard < ApplicationRecord
   has_many :sample_controlled_vocabs, inverse_of: :repository_standard, dependent: :destroy
   validates :title, presence: true
-  validates :title, uniqueness: { scope: :group_tag }
+  validates :title, uniqueness: { scope: :group }
+
+  accepts_nested_attributes_for :template_attributes, allow_destroy: true
+
+  def can_delete?(user = User.current_user)
+    return false if user.nil? || user.person.nil? 
+    # return true if user.is_admin?
+    contributor == user.person || projects.detect { |project| project.can_manage?(user) }.present?
+    contributor && sample_types.empty?
+  end
+
+  def can_view?(user = User.current_user)
+    (user && user.person && (user.person.projects & projects).any?)
+  end
+
 end
